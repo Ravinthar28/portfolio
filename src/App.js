c36058a56d627c8c09094008a9b50bef8e4064df
Ravinthar28 <131782227+Ravinthar28@users.noreply.github.com>
2025-02-03 16:13:21 +0530

    skill page working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
+// FOR ANIMATION
 import { useEffect } from "react";
 import WOW from "wow.js";
 import "../node_modules/animate.css/animate.min.css";
 
-
+// CSS FILES
 import './App.css';
+
+// COMPONENTS
 import { Home } from './homeComp/Home';
 import { Header } from "./headerComp/Header";
 import { About } from "./aboutComp/About";
 import { Project } from "./projectsComp/Project";
+import { Skills } from "./skillsComp/Skills";
 
 function App() {
 
@@ -23,6 +27,7 @@ function App() {
       <Home />
       <About />
       <Project />
+      <Skills />
     </div>
   );
 }
---
+++ src/skillsComp/Skills.jsx
@@ -0,0 +1,11 @@
+
+// CSS FILES
+import "./css/skills.css";
+
+export function Skills(){
+    return(
+        <div className="skills-container">
+        
+        </div>
+    )
+}
